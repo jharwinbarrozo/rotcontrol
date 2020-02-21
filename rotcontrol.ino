38340bf049cb0df9fbecdfc08d1a2af29cad97b1
Jharwin Barrozo <jharwinbarrozo@gmail.com>
2020-02-22 04:04:04 +0800

    9600 baud

--- rotcontrol.ino
+++ rotcontrol.ino
@@ -388,7 +388,7 @@ void setup() {
   //Reset the rotator and load configuration from EEPROM
   reset(true);
   //Initialize the serial port
-  SerialPort.begin(19200);
+  SerialPort.begin(9600);
   //Initialize the sensor
   lsm.begin();
 }
